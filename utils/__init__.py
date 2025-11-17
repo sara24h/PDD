66c206572fe0afe15b0101c8c603e2123353270b
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-11-17 23:25:45 +0330

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,14 +1,38 @@
+"""
+Utils package for PDD (Pruning During Distillation)
+Contains utilities for data loading, training, pruning, and helpers
+"""
+
 from .data_loader import get_cifar10_dataloaders
-from .trainer import PDDTrainer
+from .trainer import PDDTrainer, ApproxSign
 from .pruner import ModelPruner
-from .helpers import set_seed, save_checkpoint, load_checkpoint, count_parameters
+from .helpers import (
+    set_seed,
+    save_checkpoint,
+    load_checkpoint,
+    count_parameters,
+    get_lr,
+    AverageMeter
+)
 
 __all__ = [
+    # Data loading
     'get_cifar10_dataloaders',
+    
+    # Training
     'PDDTrainer',
+    'ApproxSign',
+    
+    # Pruning
     'ModelPruner',
+    
+    # Helpers
     'set_seed',
     'save_checkpoint',
     'load_checkpoint',
-    'count_parameters'
+    'count_parameters',
+    'get_lr',
+    'AverageMeter'
 ]
+
+__version__ = '1.0.0'
