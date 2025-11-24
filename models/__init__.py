24340cd91ea110d82e5eaed2a13e244ed645e1d3
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-11-24 21:19:23 +0330

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,5 @@
-"""
-Models package for PDD (Pruning During Distillation)
-Contains ResNet architectures for CIFAR10
-"""
+from .resnet import resnet18, resnet50, ResNet, BasicBlock
 
-from .resnet import resnet20, resnet56, ResNet, BasicBlock
-
-__all__ = [
-    'resnet20',
-    'resnet56', 
-    'ResNet',
-    'BasicBlock'
-]
+__all__ = ['resnet18', 'resnet50', 'ResNet', 'BasicBlock']
 
 __version__ = '1.0.0'
